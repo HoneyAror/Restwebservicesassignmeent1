b7fbb398dae62501a29bd2e33be32cf91b1a3fc1
honey <honey.arora@tothenew.com>
2022-03-08 12:08:44 +0530

    Added

---
+++ restWebServices/QUES9/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.example.Restwebservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeDaoService service;
+    @GetMapping(path = "/employee")
+    //method to handle all get request
+    public List<EmployeeBean> getallemployee(){
+        return service.findallemp();
+    }
+    @GetMapping(path ="/employee/{id}")
+    public EmployeeBean getemployee(@PathVariable int id){
+        EmployeeBean employeeBean=service.findone(id);
+        if(employeeBean==null)
+            throw new EmployeeNotFoundException("id-"+id);
+        return employeeBean;
+    }
+    //Create new user
+    //input:details of user
+    //output:created emp and its uri
+    @PostMapping("/employee")
+    public void CreateEmp(@Valid @RequestBody EmployeeBean employeeBean){
+        EmployeeBean saveEmp=service.SaveEmp(employeeBean);
+    }
+    @DeleteMapping(path ="/employee/{id}")
+    public void Deleteemployee(@PathVariable int id){
+        EmployeeBean employeeBean=service.Deletebyid(id);
+        if(employeeBean==null)
+            throw new EmployeeNotFoundException("id-"+id);
+    }
+
+}
